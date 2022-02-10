e14d925399b9e5641073c19546853ffe8d1af03a
Sripriya2 <99410631+Sripriya2@users.noreply.github.com>
2022-02-10 21:57:41 +0530

    Add files via upload

---
+++ EmployeeRecord.java
@@ -0,0 +1,19 @@
+package com.assignmentemployee;
+
+public class EmployeeRecord {
+	static void displayEmp(String A) {
+		System.out.println( "employee name "+ A);
+		
+	}
+	static void employeeRe(String B) {
+		System.out.println(B + " employee added");
+	}
+
+	public static void main(String[] args) {
+		displayEmp("Ravi");
+		employeeRe("vishnu");
+		// TODO Auto-generated method stub
+
+	}
+
+}
